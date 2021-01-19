adbd74d15bf38636c2b3c8dbfa2863f6e8d7fbb5
evvol <Keshavoloshin@mail.ru>
2021-01-19 19:52:09 +0300

    Lesson1

--- cloud-client/src/main/java/ClientApp.java
+++ cloud-client/src/main/java/ClientApp.java
@@ -1,2 +1,90 @@
+import javax.swing.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClientApp {
+    private final String SERVER_ADDR = "localhost";
+    private final int SERVER_PORT = 8189;
+    private Scanner scanner;
+
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public ClientApp() {
+        try {
+            openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void openConnection() throws IOException {
+        socket = new Socket(SERVER_ADDR, SERVER_PORT);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        String strFromServer = in.readUTF();
+                        if (strFromServer.equalsIgnoreCase("/end")) {
+                            break;
+                        }
+                        System.out.println(strFromServer);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void closeConnection() {
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage() {
+
+            try {
+                out.writeUTF("Hello");
+            } catch (IOException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Ошибка отправки сообщения");
+            }
+        }
+
+
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientApp();
+            }
+        });
+    }
 }
+
+
